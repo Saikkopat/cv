cb65d0e30480319ec2d506ac4a5739b8ad93eaa5
Angel Cardenas <maskemaneac@gmail.com>
2020-11-21 07:43:35 -0600

    Rebuild of the cv site with React.JS and react-bootstrap

---
+++ src/MyNavbar.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import {Navbar, Nav } from 'react-bootstrap';
+import RedLanternIcon from "./favicon.ico";
+class MyNavbar extends Component {
+  render() {
+    return (
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top" position="fixed">
+        <Navbar.Brand href="#home">A <img src={RedLanternIcon} width="30" heigt="30" alt="Red Lanter Corps icon"/> Cardenas</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#about-me">About me</Nav.Link>
+            <Nav.Link href="#pricing">Education</Nav.Link>
+            <Nav.Link href="#pre-exp">Previous works</Nav.Link>
+            <Nav.Link href="#proyects">Proyects</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#contact">Contact me</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default MyNavbar;
---
+++ src/Title.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Title.css";
+
+class Title extends Component {
+  render() {
+    return (
+          <div id="home">
+              <h1 className="centralTitle title">Angel Cardenas</h1>
+              <h2 className="subTitle title">A (in a strange way) programmer.</h2>
+          </div>
+    );
+  }
+}
+
+export default Title;
